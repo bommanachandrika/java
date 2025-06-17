5304ccf6e4bc8716d1bcebb8ce749701f02efb63
bommanachandrika <168529556+bommanachandrika@users.noreply.github.com>
2025-06-17 18:44:37 +0530

    Add files via upload

---
+++ RomanToInteger.java
@@ -0,0 +1,36 @@
+public class RomanToInteger {
+    public int romanToInt(String s) {
+        int result = 0;
+        int prevalue = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int value = getValue(s.charAt(i));
+            if (value >= prevalue) {
+                result = result + value;
+            } else {
+                result = result - value;
+            }
+            prevalue = value;
+        }
+        return result;
+    }
+
+    private static int getValue(char c) {
+        switch (c) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
+    }
+
+    public static void main(String args[]) {
+        RomanToInteger sol = new RomanToInteger();
+        String roman = "MCMXCIV";
+        int result = sol.romanToInt(roman);
+        System.out.println("The integer value of " + roman + " is: " + result);
+    }
+}
